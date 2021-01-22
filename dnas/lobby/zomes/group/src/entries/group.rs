fb0e32a680c1709815285e1390c53706eeb2bb0d
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-01-22 16:28:28 +0900

    added commnets

--- dnas/lobby/zomes/group/src/entries/group.rs
+++ dnas/lobby/zomes/group/src/entries/group.rs
@@ -16,6 +16,7 @@ pub struct AgentPubKeys(Vec<AgentPubKey>);
 #[derive(Deserialize, Serialize, SerializedBytes, Clone)]
 pub struct AddInitialMembersInput {
     invitee: Vec<AgentPubKey>,
+    // TATS: this should be HeaderHash if the archi changes push through.
     group_entry_hash: EntryHash,
     secret_hash: SecretHash
 }
@@ -35,6 +36,8 @@ entry_def!(Group
     }
 );
 #[derive(Debug, Clone, Serialize, Deserialize, SerializedBytes)]
+// TATS: this may be gone and we may just use XSalsa20Poly1305KeyRef as it is
+// if the archi changes push through.
 pub struct SecretHash(pub XSalsa20Poly1305KeyRef);
 
 #[derive(Debug, Clone, Serialize, Deserialize, SerializedBytes)]
@@ -54,7 +57,9 @@ entry_def!(GroupSecretKey
 
 #[derive(Debug, Clone, Serialize, Deserialize, SerializedBytes)]
 pub struct GroupMembers {
+    // TATS: should be HeaderHash if the architecture changes pushes through. 
     pub group_hash: EntryHash, // this should not change -> checked with validation
+    // TATS: this will be taken out if the architectural changes push through.
     pub secret_hash: SecretHash,//this is the hash where the encription_key its storages
     pub members: Vec<AgentPubKey>
 }
