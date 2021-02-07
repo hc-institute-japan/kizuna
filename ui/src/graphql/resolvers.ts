42fc4ad7abfd2da28f4a4240e73897e7df746786
seulgibear <neilsongardose@gmail.com>
2021-02-08 02:10:05 +0800

    Remove all GraphQL related files

--- ui/src/graphql/resolvers.ts
+++
@@ -1,6 +0,0 @@
-import ProfileResolvers from "./profile/resolvers";
-import ContactsResolvers from "./contacts/resolvers";
-
-const resolvers = [ProfileResolvers, ContactsResolvers];
-
-export default resolvers;
