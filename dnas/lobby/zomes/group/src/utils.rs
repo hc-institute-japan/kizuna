3d1ac0c11970f8c7d4e798fd3441a8c7a652cea9
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-02-10 22:48:29 +0900

    fmt

--- dnas/lobby/zomes/group/src/utils.rs
+++ dnas/lobby/zomes/group/src/utils.rs
@@ -1,13 +1,9 @@
-use std::time::Duration;
 use hdk3::prelude::*;
+use std::time::Duration;
 
 use timestamp::Timestamp;
 
-
-
-use crate::entries::group::{
-    BlockedWrapper,
-};
+use crate::entries::group::BlockedWrapper;
 
 pub(crate) fn to_timestamp(duration: Duration) -> Timestamp {
     Timestamp(duration.as_secs() as i64, duration.subsec_nanos())
@@ -19,19 +15,12 @@ pub(crate) fn get_my_blocked_list() -> HdkResult<BlockedWrapper> {
     let function_name: FunctionName = FunctionName("list_blocked".to_owned());
 
     let my_blocked_list: BlockedWrapper = call(
-        None,// The cell you want to call (If None will call the current cell).
+        None, // The cell you want to call (If None will call the current cell).
         zome_name,
         function_name,
         None, //The capability secret if required.
-        &()   //This are the input value we send to the fuction we are calling
+        &(),  //This are the input value we send to the fuction we are calling
     )?;
 
     Ok(my_blocked_list)
 }
-
-
-
-
-
-
-
