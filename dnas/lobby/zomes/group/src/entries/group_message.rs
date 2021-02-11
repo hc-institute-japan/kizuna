21e781358d066db9ba574bcc399cfc06b8725e35
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-02-11 15:23:32 +0900

    added comments for send_message

--- dnas/lobby/zomes/group/src/entries/group_message.rs
+++ dnas/lobby/zomes/group/src/entries/group_message.rs
@@ -1,11 +1,15 @@
 use hdk3::prelude::timestamp::Timestamp;
 use hdk3::prelude::*;
 // found in ../../commons/file_types
-// use file_types::*;
+// TODO: use this instead of defining file payload related types here.
+// use file_types::{
+// whatever type you need here
+// };
 
 pub mod handlers;
 
 //ENUMS
+// TODO: this is already defined in the commoncs/file_types so delete this and use that instead
 #[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
 pub enum PayloadType {
     Text,
@@ -13,23 +17,22 @@ pub enum PayloadType {
     All,
 }
 
+// TODO: this is already defined in the commoncs/file_types so delete this and use that instead
 #[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
 enum Payload {
     Text {
         payload: String,
     },
-    // do we still separate by file type (e.g. photo, video, document)?
     File {
         file_name: String,
         file_size: usize,
-        // if the file types accomodated is sure,
-        // we can have this as enum
         file_type: String,
         bytes: Vec<u8>, // actual bytes of file (15mb limit)
     },
 }
 //GROUP MESSAGE TYPE DEFINITION, GETTERS, SETTERS, ENTRY_DEF, UTILS ...
 #[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct GroupMessage {
     // EntryHash of first ver of Group
     group_hash: EntryHash,
@@ -50,10 +53,13 @@ entry_def!(GroupMessage
 );
 //END OF GROUP MESSAGE TYPE DEFINITION
 
+// TODO: Define the GroupFileBytes.
+
 //START OF INPUTS TYPES DEFINITION
 #[derive(Serialize, Deserialize, SerializedBytes, Clone)]
 pub struct GroupMessageInput {
     group_hash: EntryHash,
+    // TODO: change this to payload_input: PayloadInput(found in commons/file_types)
     payload: Payload,
     sender: AgentPubKey,
     reply_to: Option<EntryHash>,
@@ -73,6 +79,7 @@ pub struct GroupChatFilter {
 
 //OUTPUTS TYPES DEFINITION
 #[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct GroupMessageData {
     // entry_hash of GroupMessage
     id: EntryHash,
