01240316efa9e72abdf2d48cc36b14db3c909bad
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-02-11 22:59:21 +0900

    test for indicate_group_typing done

--- dnas/lobby/tests/src/zomes/group/index.ts
+++ dnas/lobby/tests/src/zomes/group/index.ts
@@ -5,14 +5,17 @@ import {
   validateCreateGroupTest,
 } from "./group";
 
+import { groupTypingIndicatorTest } from "./group_message";
+
 const delay = (ms) => new Promise((r) => setTimeout(r, ms));
 
 export default (orchestrator, config, installables) => {
   // Group CRUD and validation related tests.
-  craeteGroupTest(orchestrator, config, installables);
-  addAndRemoveMembersTest(orchestrator, config, installables);
-  updateGroupNameTest(orchestrator, config, installables);
-  validateCreateGroupTest(orchestrator, config, installables);
+  //   craeteGroupTest(orchestrator, config, installables);
+  //   addAndRemoveMembersTest(orchestrator, config, installables);
+  //   updateGroupNameTest(orchestrator, config, installables);
+  //   validateCreateGroupTest(orchestrator, config, installables);
 
   // GroupMessage related tests
+  groupTypingIndicatorTest(orchestrator, config, installables);
 };
--- dnas/lobby/tests/src/zomes/group/zome_fns.ts
+++ dnas/lobby/tests/src/zomes/group/zome_fns.ts
@@ -1,38 +1,49 @@
 export function init(conductor) {
-    conductor.call("group","init",);
+  conductor.call("group", "init");
 }
 
 export function createGroup(create_group_input) {
-    return (conductor) => conductor.call("group", "create_group", create_group_input);
+  return (conductor) =>
+    conductor.call("group", "create_group", create_group_input);
 }
 
 export function AddGroupMebers(update_members_io) {
-    return (conductor)=>
-    conductor.call("group","add_members", update_members_io);
+  return (conductor) =>
+    conductor.call("group", "add_members", update_members_io);
 }
 
 export function removeGroupMembers(remove_members_io) {
-    return (conductor)=>
-    conductor.call("group","remove_members", remove_members_io);
+  return (conductor) =>
+    conductor.call("group", "remove_members", remove_members_io);
 }
 
 export function getLatestGroupVersion(group_entry_hash) {
-    return (coductor)=>
-    coductor.call("group","get_group_latest_version",group_entry_hash);
+  return (coductor) =>
+    coductor.call("group", "get_group_latest_version", group_entry_hash);
 }
 
 export function updateGroupName(update_group_name_io) {
-    return (conductor) => 
-    conductor.call("group","update_group_name", update_group_name_io); 
+  return (conductor) =>
+    conductor.call("group", "update_group_name", update_group_name_io);
 }
 
 export function getMyGroupsList(conductor) {
-    return conductor.call("group","get_all_my_groups",);
+  return conductor.call("group", "get_all_my_groups", null);
+}
+
+// Group Message related functions
+export function indicateGroupTyping(group_typing_detail_data) {
+  return (conductor) =>
+    conductor.call("group", "indicate_group_typing", group_typing_detail_data);
+}
+
+export function readGroupMessage(group_message_read_io) {
+  return (conductor) =>
+    conductor.call("group", "read_group_message", group_message_read_io);
 }
 
 export function signalHandler(signal, signal_listener) {
-
-    /*
+  /*
     //this is the incoming signal format
     signal = { 
         type: String, 
@@ -44,18 +55,17 @@ export function signalHandler(signal, signal_listener) {
             } 
         }
     }
-    */ 
+    */
 
-    signal_listener.counter++;
-    return (payload) =>{
+  signal_listener.counter++;
+  return (payload) => {
+    console.log(`we have a new signal incoming ${payload}`);
+    signal_listener.payload = signal.data.payload.payload;
+  };
+}
 
-        console.log(`we have a new signal incoming ${payload}`);
-        signal_listener.payload = signal.data.payload.payload;
-    }
-};
-
-// VAlIDATION FUCNTIONS 
+// VAlIDATION FUCNTIONS
 export function runValidationRules(validation_input) {
-    return (conductor)=>
-        conductor.call("group","run_validation",validation_input);
+  return (conductor) =>
+    conductor.call("group", "run_validation", validation_input);
 }
