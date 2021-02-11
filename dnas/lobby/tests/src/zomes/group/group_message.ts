22a7c7f1fe27682da9ec814dc4f73ca68749a165
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-02-11 21:42:50 +0900

    boilerplate for group_read_message and inidicate_group_typing test

--- dnas/lobby/tests/src/zomes/group/group_message.ts
+++ dnas/lobby/tests/src/zomes/group/group_message.ts
@@ -1,13 +1,19 @@
-import { ScenarioApi } from "@holochain/tryorama/lib/api";
-
 const delay = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export default (orchestrator, config, installables) => {
-
-  orchestrator.registerScenario("test typing indicator for group chat", async(s,t) =>{
-
+export function groupTypingIndicatorTest(orchestrator, config, installables) {
+  orchestrator.registerScenario(
+    "test typing indicator for group chat",
+    async (s, t) => {
       // TODO: tests
+    }
+  );
+}
 
-  });
-
+export function readGroupMessageTest(orchestrator, config, installables) {
+  orchestrator.registerScenario(
+    "test read message for group chat",
+    async (s, t) => {
+      // TODO: tests
+    }
+  );
 }
