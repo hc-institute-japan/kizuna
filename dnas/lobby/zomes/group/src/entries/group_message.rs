a553d4886a51b0dd9d33eb3893136eac1b2a80df
seulgibear <neilsongardose@gmail.com>
2021-02-11 08:14:20 +0800

    Implement initial group messaging send_message and get_all_messages function

--- dnas/lobby/zomes/group/src/entries/group_message.rs
+++ dnas/lobby/zomes/group/src/entries/group_message.rs
@@ -1,2 +1,64 @@
-#![allow(unused_imports)]
+use hdk3::prelude::timestamp::Timestamp;
 use hdk3::prelude::*;
+
+pub mod handlers;
+
+//ENUMS
+#[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+enum Payload {
+    Text {
+        payload: String,
+    },
+    // do we still separate by file type (e.g. photo, video, document)?
+    File {
+        file_name: String,
+        file_size: usize,
+        // if the file types accomodated is sure,
+        // we can have this as enum
+        file_type: String,
+        bytes: Vec<u8>, // actual bytes of file (15mb limit)
+    },
+}
+//GROUP MESSAGE TYPE DEFINITION, GETTERS, SETTERS, ENTRY_DEF, UTILS ...
+#[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+pub struct GroupMessage {
+    // EntryHash of first ver of Group
+    group_hash: EntryHash,
+    payload: Payload,
+    created: Timestamp,
+    sender: AgentPubKey,
+    reply_to: Option<EntryHash>,
+}
+
+entry_def!(GroupMessage
+    EntryDef {
+        id: "group_message".into(),
+        visibility: EntryVisibility::Public,
+        crdt_type: CrdtType,
+        required_validations: RequiredValidations::default(),
+        required_validation_type: RequiredValidationType::Element
+    }
+);
+//END OF GROUP MESSAGE TYPE DEFINITION
+
+//START OF INPUTS TYPES DEFINITION
+#[derive(Serialize, Deserialize, SerializedBytes, Clone)]
+pub struct GroupMessageInput {
+    group_hash: EntryHash,
+    payload: Payload,
+    sender: AgentPubKey,
+    reply_to: Option<EntryHash>,
+}
+//END OF INPUTS TYPES DEFINITION
+
+//OUTPUTS TYPES DEFINITION
+#[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+pub struct GroupMessageData {
+    // entry_hash of GroupMessage
+    id: EntryHash,
+    content: GroupMessage,
+}
+
+#[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+pub struct GroupMessageDataWrapper(Vec<GroupMessageData>);
+//END OF OUTPUTS TYPES DEFINITION
--- dnas/lobby/zomes/group/src/utils.rs
+++ dnas/lobby/zomes/group/src/utils.rs
@@ -1,13 +1,9 @@
-use std::time::Duration;
 use hdk3::prelude::*;
+use std::time::Duration;
 
 use timestamp::Timestamp;
 
-
-
-use crate::entries::group::{
-    BlockedWrapper,
-};
+use crate::entries::group::BlockedWrapper;
 
 pub(crate) fn to_timestamp(duration: Duration) -> Timestamp {
     Timestamp(duration.as_secs() as i64, duration.subsec_nanos())
@@ -19,19 +15,37 @@ pub(crate) fn get_my_blocked_list() -> HdkResult<BlockedWrapper> {
     let function_name: FunctionName = FunctionName("list_blocked".to_owned());
 
     let my_blocked_list: BlockedWrapper = call(
-        None,// The cell you want to call (If None will call the current cell).
+        None, // The cell you want to call (If None will call the current cell).
         zome_name,
         function_name,
         None, //The capability secret if required.
-        &()   //This are the input value we send to the fuction we are calling
+        &(),  //This are the input value we send to the fuction we are calling
     )?;
 
     Ok(my_blocked_list)
 }
 
+const SECONDS: i64 = 60;
+const MINUTES: i64 = 60;
+const HOURS: i64 = 24;
 
+pub fn path_from_str(str: &str) -> Path {
+    let path = Path::from(str);
+    path.ensure().expect("Cannot ensure path.");
+    path
+}
 
+pub fn timestamp_to_days(seconds: Timestamp) -> i64 {
+    seconds.0 / (SECONDS * MINUTES * HOURS)
+}
 
+pub fn error<T>(code: &str, message: &str) -> ExternResult<T> {
+    Err(HdkError::Wasm(WasmError::Zome(format!(
+        "{{\"code\": \"{}\", \"message\": \"{}\"}}",
+        code, message
+    ))))
+}
 
-
-
+pub fn collect<A, B>(vec: Vec<A>, callback: fn(A) -> HdkResult<B>) -> HdkResult<Vec<B>> {
+    vec.into_iter().map(|item| callback(item)).collect()
+}
