0260bc95cb65a24c55dd4267b586d03291e45603
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-02-13 16:43:08 +0900

    edited send_message zome fn and test
    - changed the place where you run clone() when you only need a specific field from a struct
    - took out match cases when it can be handled with ? operator

--- dnas/lobby/tests/src/zomes/group/index.ts
+++ dnas/lobby/tests/src/zomes/group/index.ts
@@ -11,10 +11,10 @@ const delay = (ms) => new Promise((r) => setTimeout(r, ms));
 
 export default (orchestrator, config, installables) => {
   // Group CRUD and validation related tests.
-  craeteGroupTest(orchestrator, config, installables);
-  addAndRemoveMembersTest(orchestrator, config, installables);
-  updateGroupNameTest(orchestrator, config, installables);
-  validateCreateGroupTest(orchestrator, config, installables);
+  // craeteGroupTest(orchestrator, config, installables);
+  // addAndRemoveMembersTest(orchestrator, config, installables);
+  // updateGroupNameTest(orchestrator, config, installables);
+  // validateCreateGroupTest(orchestrator, config, installables);
 
   // GroupMessage related tests
   groupTypingIndicatorTest(orchestrator, config, installables);
