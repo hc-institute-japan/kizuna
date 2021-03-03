208370c5facfbb4cdd43edaf8efab7914611d47b
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-03-03 19:35:01 +0900

    changed signal payload for AddedToGroup

--- dnas/lobby/tests/src/zomes/group/index.ts
+++ dnas/lobby/tests/src/zomes/group/index.ts
@@ -18,16 +18,15 @@ import {
 
 export default (orchestrator, config, installables) => {
   // Group CRUD and validation related tests.
-  //createGroupTest(orchestrator, config, installables);
-  //addAndRemoveMembersTest(orchestrator, config, installables);
-  //updateGroupNameTest(orchestrator, config, installables);
-  //validateCreateGroupTest(orchestrator, config, installables);
+  createGroupTest(orchestrator, config, installables);
+  addAndRemoveMembersTest(orchestrator, config, installables);
+  updateGroupNameTest(orchestrator, config, installables);
+  validateCreateGroupTest(orchestrator, config, installables);
 
   // GroupMessage related tests
-  //getLatestMessagesForAllGroupsTest(orchestrator, config, installables);
-  //getMessagesByGroupByTimestampTest(orchestrator, config, installables);
-  //sendMessageTest(orchestrator, config, installables);
-  //sendMessageswithFilesTest(orchestrator, config, installables);
+  getLatestMessagesForAllGroupsTest(orchestrator, config, installables);
+  getMessagesByGroupByTimestampTest(orchestrator, config, installables);
+  sendMessageTest(orchestrator, config, installables);
+  sendMessageswithFilesTest(orchestrator, config, installables);
   sendLargeSetOfFilesTest(orchestrator, config, installables);
-
 };
