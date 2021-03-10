b56629315abd3af61516f0134a688c39f7a7e987
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-03-10 14:48:46 +0900

    minor edit

--- dnas/lobby/tests/src/zomes/group/index.ts
+++ dnas/lobby/tests/src/zomes/group/index.ts
@@ -24,13 +24,13 @@ export default (orchestrator, config, installables) => {
   updateGroupNameTest(orchestrator, config, installables);
   validateCreateGroupTest(orchestrator, config, installables);
   // GroupMessage related tests
-  getMessagesByGroupByTimestampTest(orchestrator, config, installables); // good
   sendMessageTest(orchestrator, config, installables); // good
+  sendMessageswithFilesTest(orchestrator, config, installables); // good
+  sendLargeSetOfFilesTest(orchestrator, config, installables); // good
+  getNextBatchOfMessagesTest(orchestrator, config, installables); // good
+  getMessagesByGroupByTimestampTest(orchestrator, config, installables); // good
   getLatestMessagesForAllGroupsTest(orchestrator, config, installables); // good
   fetchFilesForAParticularDateTest(orchestrator, config, installables); // good
-  sendMessageswithFilesTest(orchestrator, config, installables); // good
   groupTypingIndicatorTest(orchestrator, config, installables); // good
-  readGroupMessageTest(orchestrator, config, installables);
-  getNextBatchOfMessagesTest(orchestrator, config, installables); // good
-  sendLargeSetOfFilesTest(orchestrator, config, installables); // good
+  readGroupMessageTest(orchestrator, config, installables); // good
 };
