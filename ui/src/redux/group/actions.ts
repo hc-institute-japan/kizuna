6412679cd4b9e70ab74516a2f88122dddbb7fd08
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-03-10 15:12:44 +0900

    move sendInitialMessage to group redux

--- ui/src/redux/group/actions.ts
+++ ui/src/redux/group/actions.ts
@@ -1,5 +1,7 @@
 import { FUNCTIONS, ZOMES } from "../../connection/types";
 import { ThunkAction } from "../types";
+import { Uint8ArrayToBase64 } from "../../utils/helpers";
+import { Profile } from "../profile/types";
 import {
   ADD_GROUP,
   ADD_MEMBERS,
@@ -188,6 +190,85 @@ export const sendGroupMessage = (
 
   return groupMessageData;
 };
+
+// export const sendInitialGroupMessage = (
+//   members: Profile[],
+//   message: string
+// ): ThunkAction => async (dispatch, getState, { callZome }) => {
+//   const { conversations, messages } = getState().groupConversations;
+//   const name = members.map((member) => member.username).join(", ");
+//   const groupRes = await callZome({
+//     zomeName: ZOMES.GROUP,
+//     fnName: FUNCTIONS[ZOMES.GROUP].CREATE_GROUP,
+//     payload: {
+//       name,
+//       members: members.map((member) => member.id),
+//     },
+//   });
+//   if (groupRes?.type !== "error") {
+//     const date = new Date(groupRes.content.created[0] * 1000);
+
+//     const messageRes = await callZome({
+//       zomeName: ZOMES.GROUP,
+//       fnName: FUNCTIONS[ZOMES.GROUP].SEND_MESSAGE,
+//       payload: {
+//         sender: groupRes.content.creator,
+//         groupHash: groupRes.groupId,
+//         payloadInput: {
+//           type: "TEXT",
+//           payload: {
+//             payload: message,
+//           },
+//         },
+//       },
+//     });
+//     if (messageRes?.type !== "error") {
+//       const groupConversation: GroupConversation = {
+//         originalGroupEntryHash: groupRes.groupId,
+//         originalGroupHeaderHash: groupRes.groupRevisionId,
+//         createdAt: date,
+//         creator: groupRes.content.creator,
+//         messages: [Uint8ArrayToBase64(messageRes.id)],
+//         versions: [
+//           {
+//             groupEntryHash: groupRes.groupId,
+//             name,
+//             conversants: members.map((contact) =>
+//               Uint8ArrayToBase64(contact.id)
+//             ),
+//             timestamp: date,
+//           },
+//         ],
+//       };
+//       const groupMessage: GroupMessage = {
+//         groupMessageEntryHash: messageRes.id,
+//         groupEntryHash: groupRes.groupId,
+//         author: groupRes.content.creator,
+//         payload: {
+//           payload: message,
+//         },
+//         timestamp: new Date(messageRes.content.created[0] * 1000),
+//         readList: {},
+//       };
+//       dispatch({
+//         type: SET_CONVERSATIONS,
+//         conversations: {
+//           ...conversations,
+//           [Uint8ArrayToBase64(groupRes.groupId)]: groupConversation,
+//         },
+//       });
+//       dispatch({
+//         type: SET_MESSAGES,
+//         messages: {
+//           ...messages,
+//           [Uint8ArrayToBase64(messageRes.id)]: groupMessage,
+//         },
+//       });
+//       return groupConversation;
+//     }
+//   }
+//   return false;
+// };
 
 export const getNextBatchGroupMessages = (): ThunkAction => async (
   dispatch,
