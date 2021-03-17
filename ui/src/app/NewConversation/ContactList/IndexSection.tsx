152672549e35772add68d4a44d82d264e7fac05d
seulgibear <neilsongardose@gmail.com>
2021-03-17 14:44:53 +0800

    Fix new conversation modal bug

--- ui/src/app/NewConversation/ContactList/IndexSection.tsx
+++ ui/src/app/NewConversation/ContactList/IndexSection.tsx
@@ -15,14 +15,14 @@ const IndexSection: React.FC<Props> = ({ char, contacts }) => {
   const setSelectedContacts = useContacts()[3];
 
   const handleOnClick = (contact: Profile) => {
-    // setSelectedContacts((currContacts) => {
-    //   currContacts[Uint8ArrayToBase64(contact.id)] = contact;
-    //   return currContacts;
-    // });
-    // setContacts((currContacts) => {
-    //   delete currContacts[Uint8ArrayToBase64(contact.id)];
-    //   return { ...currContacts };
-    // });
+    setSelectedContacts((currContacts) => {
+      currContacts[contact.id] = contact;
+      return currContacts;
+    });
+    setContacts((currContacts) => {
+      delete currContacts[contact.id];
+      return { ...currContacts };
+    });
   };
 
   return (
