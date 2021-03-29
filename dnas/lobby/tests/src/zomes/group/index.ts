0c71d961f1f43fa6a5e1008cd347306b3a552da9
Manuel <manuel.r.puentes.d@gmail.com>
2021-03-28 23:43:47 -0400

    group_zome bumped into hdk 0.0.100 2 tests failing

--- dnas/lobby/tests/src/zomes/group/index.ts
+++ dnas/lobby/tests/src/zomes/group/index.ts
@@ -25,17 +25,15 @@ export default (config, installables) => {
   // updateGroupNameTest(config, installables); // good
   // validateCreateGroupTest(config, installables); //good
   
-  
   // GroupMessage related tests
+  // sendMessageTest(config, installables); // good
+  // sendMessageswithFilesTest(config, installables); // good
+  // sendLargeSetOfFilesTest(config, installables); // good
+  // getNextBatchOfMessagesTest(config, installables); // good
+  // getMessagesByGroupByTimestampTest(config, installables); // good
+  // getLatestMessagesForAllGroupsTest(config, installables); // good
+  // fetchFilesForAParticularDateTest(config, installables); // good
   
-  
-  // sendMessageTest(orchestrator, config, installables); // good
-  // sendMessageswithFilesTest(orchestrator, config, installables); // good
-  // sendLargeSetOfFilesTest(orchestrator, config, installables); // good
-  // getNextBatchOfMessagesTest(orchestrator, config, installables); // good
-  // getMessagesByGroupByTimestampTest(orchestrator, config, installables); // good
-  // getLatestMessagesForAllGroupsTest(orchestrator, config, installables); // good
-  // fetchFilesForAParticularDateTest(orchestrator, config, installables); // good
-  // groupTypingIndicatorTest(orchestrator, config, installables); // good
-  // readGroupMessageTest(orchestrator, config, installables); // good
+  // groupTypingIndicatorTest(config, installables); // not-goodk
+  // readGroupMessageTest(config, installables); // not-good
 };
