d2ebcdaa816a6685320cfcc0f6fc4d9a07136e92
seulgibear <neilsongardose@gmail.com>
2021-04-06 16:29:51 +0800

    Reimplement grid in media tab within group chat info page

--- ui/src/app/GroupChat/GroupChatInfo/Tabs/Media/MediaItem.tsx
+++ ui/src/app/GroupChat/GroupChatInfo/Tabs/Media/MediaItem.tsx
@@ -1,46 +1,37 @@
 import { IonItem } from "@ionic/react";
-  import React from "react";
+import React from "react";
 import { FilePayload } from "../../../../../redux/commons/types";
 import ImageView from "../../../../../components/Chat/File/ImageView/index";
 import VideoView from "../../../../../components/Chat/File/VideoView";
-  
-  interface Props {
-    file?: FilePayload;
-  }
-  
-  const MediaItem: React.FC<Props> = ({ file }) => {
-    // const [selectedItem, setSelectedItem] = useState<boolean>(false);
-    const decoder = new TextDecoder();
 
-    const renderFile = () => {
-        switch (file?.fileType) {
-          case "IMAGE":
-            return (
-              <ImageView file={file} src={decoder.decode(file.thumbnail!)} />
-            );
-          case "VIDEO":
-            // TODO: fix this
-            return (
-              <VideoView file={file} />
-            )
-          case "OTHER":
-            return null;
-          default:
-            return null;
-        }
-      };
-  
-    // const handleOnClick = () => {
-    //   let selected = onCompletion(contact);
-    //   if (selected) setSelectedItem(selectedItem ? false : true);
-    // };
-  
-    return (
-      <IonItem button　lines="none" key={JSON.stringify(file?.fileHash)} >
-          {renderFile()}
-      </IonItem>
-    );
+interface Props {
+  file?: FilePayload;
+}
+
+const MediaItem: React.FC<Props> = ({ file }) => {
+  // const [selectedItem, setSelectedItem] = useState<boolean>(false);
+  const decoder = new TextDecoder();
+
+  const renderFile = () => {
+    switch (file?.fileType) {
+      case "IMAGE":
+        return <ImageView file={file} src={decoder.decode(file.thumbnail!)} />;
+      case "VIDEO":
+        // TODO: fix this
+        return <VideoView file={file} />;
+      case "OTHER":
+        return null;
+      default:
+        return null;
+    }
   };
-  
-  export default MediaItem;
-  
+
+  // const handleOnClick = () => {
+  //   let selected = onCompletion(contact);
+  //   if (selected) setSelectedItem(selectedItem ? false : true);
+  // };
+
+  return renderFile();
+};
+
+export default MediaItem;
--- ui/src/app/GroupChat/index.tsx
+++ ui/src/app/GroupChat/index.tsx
@@ -22,12 +22,20 @@ import {
   GroupConversation,
   GroupMessageInput,
   GroupMessage,
+  UpdateGroupMembersData,
+  GroupMessageBatchFetchFilter,
+  GroupMessagesOutput,
+  GroupMessageByDateFetchFilter,
 } from "../../redux/group/types";
 import { RootState } from "../../redux/types";
-import { FilePayloadInput } from "../../redux/commons/types"; 
+import { FileMetadataInput, FilePayloadInput } from "../../redux/commons/types";
 import MessageList from "./MessageList";
-import { base64ToUint8Array, Uint8ArrayToBase64, useAppDispatch } from "../../utils/helpers";
-import {fetchId} from "../../redux/profile/actions";
+import {
+  base64ToUint8Array,
+  Uint8ArrayToBase64,
+  useAppDispatch,
+} from "../../utils/helpers";
+import { fetchId } from "../../redux/profile/actions";
 
 import MessageInput from "../../components/MessageInput";
 import { arrowBackSharp, informationCircleOutline } from "ionicons/icons";
@@ -59,13 +67,13 @@ const GroupChat: React.FC = () => {
           payload: {
             metadata: {
               fileName: file.metadata.fileName,
-              fileSize:file.metadata.fileSize,
+              fileSize: file.metadata.fileSize,
               fileType: file.metadata.fileType,
             },
             fileType: file.fileType,
             fileBytes: file.fileBytes,
-          }
-        }
+          },
+        };
         let groupMessage: GroupMessageInput = {
           groupHash: base64ToUint8Array(groupInfo!.originalGroupEntryHash),
           payloadInput: filePayloadInput,
@@ -75,24 +83,24 @@ const GroupChat: React.FC = () => {
         };
         inputs.push(groupMessage);
       });
-    };
+    }
     if (message.length) {
       inputs.push({
         groupHash: base64ToUint8Array(groupInfo!.originalGroupEntryHash),
         payloadInput: {
           type: "TEXT",
-          payload: {payload: message}
+          payload: { payload: message },
         },
         sender: Buffer.from(base64ToUint8Array(myAgentId).buffer),
         // TODO: handle replying to message here as well
         replyTo: undefined,
-      })
+      });
     }
 
     inputs.forEach((groupMessage: any) => {
       // TODO: error handling
       dispatch(sendGroupMessage(groupMessage)).then((res: GroupMessage) => {
-        groupInfo?.messages.push(res.groupMessageEntryHash)
+        groupInfo?.messages.push(res.groupMessageEntryHash);
       });
     });
   };
@@ -103,53 +111,78 @@ const GroupChat: React.FC = () => {
     });
   };
 
-
   useEffect(() => {
-  // setLoading(true);
-  dispatch(fetchId()).then((res: AgentPubKey | null) => {
-    if (res) setMyAgentId(Uint8ArrayToBase64(res))
-  });
-  }, [dispatch])
-
-  useEffect(() => {
-    if (groupData) {
-      dispatch(getLatestGroupVersion(group)).then((res:GroupConversation) => {
-        setGroupInfo(res);
-      })
-      setLoading(false);
-    } else {
-      dispatch(getLatestGroupVersion(group)).then((res:GroupConversation) => {
-        setGroupInfo(res);
-        setLoading(false);
-      })
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // setLoading(true);
+    dispatch(fetchId()).then((res: AgentPubKey | null) => {
+      if (res) setMyAgentId(Uint8ArrayToBase64(res));
+    });
   }, []);
 
   useEffect(() => {
-    groupData ? groupData.messages ?  setMessages(groupData.messages) : setMessages([]) : setMessages([])
-  }, [groupData])
+    if (groupData) {
+      dispatch(getLatestGroupVersion(group)).then((res: GroupConversation) => {
+        console.log(res);
+        setGroupInfo(res);
+      });
+      setLoading(false);
+    } else {
+      dispatch(getLatestGroupVersion(group)).then((res: GroupConversation) => {
+        console.log(res);
+        setGroupInfo(res);
+        setLoading(false);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  useEffect(() => {
+    groupData
+      ? groupData.messages
+        ? setMessages(groupData.messages)
+        : setMessages([])
+      : setMessages([]);
+  }, [groupData]);
 
-
-
-  return (!loading && groupInfo) ? (
+  return !loading && groupInfo ? (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons>
-            <IonButton  onClick={() => handleOnBack()} className="ion-no-padding" >
-              <IonIcon slot="icon-only" icon={arrowBackSharp} />  
+            <IonButton
+              onClick={() => handleOnBack()}
+              className="ion-no-padding"
+            >
+              <IonIcon slot="icon-only" icon={arrowBackSharp} />
             </IonButton>
             <IonAvatar className="ion-padding">
               {/* TODO: proper picture for default avatar if none is set */}
-              {groupInfo ? groupInfo!.avatar ? <img src={groupInfo!.avatar} alt={groupInfo!.name} /> : <img src={"https://upload.wikimedia.org/wikipedia/commons/8/8c/Lauren_Tsai_by_Gage_Skidmore.jpg"} alt={groupInfo!.name} /> : <img src={"https://upload.wikimedia.org/wikipedia/commons/8/8c/Lauren_Tsai_by_Gage_Skidmore.jpg"} alt={groupInfo!.name} />}
+              {groupInfo ? (
+                groupInfo!.avatar ? (
+                  <img src={groupInfo!.avatar} alt={groupInfo!.name} />
+                ) : (
+                  <img
+                    src={
+                      "https://upload.wikimedia.org/wikipedia/commons/8/8c/Lauren_Tsai_by_Gage_Skidmore.jpg"
+                    }
+                    alt={groupInfo!.name}
+                  />
+                )
+              ) : (
+                <img
+                  src={
+                    "https://upload.wikimedia.org/wikipedia/commons/8/8c/Lauren_Tsai_by_Gage_Skidmore.jpg"
+                  }
+                  alt={groupInfo!.name}
+                />
+              )}
             </IonAvatar>
             <IonTitle className="ion-no-padding"> {groupInfo!.name}</IonTitle>
-            <IonButton onClick={() =>  {
-              history.push(`/g/${groupInfo.originalGroupEntryHash}/info`);
-            }}>
-              <IonIcon slot ="icon-only" icon={informationCircleOutline}/>
+            <IonButton
+              onClick={() => {
+                history.push(`/g/${groupInfo.originalGroupEntryHash}/info`);
+              }}
+            >
+              <IonIcon slot="icon-only" icon={informationCircleOutline} />
             </IonButton>
           </IonButtons>
         </IonToolbar>
@@ -162,16 +195,20 @@ const GroupChat: React.FC = () => {
             members={groupInfo!.members}
             messageIds={messages}
           ></MessageList>
-        ) : <IonLoading isOpen={loading} />}
+        ) : (
+          <IonLoading isOpen={loading} />
+        )}
       </IonContent>
-      {/* BUG: the input field does not reset to empty after send */}
+
       <MessageInput
-          onSend={() => handleOnSend()}
-          onChange={(message) => setMessage(message)}
-          onFileSelect={(files) => setFiles(files)}
-        />
+        onSend={() => handleOnSend()}
+        onChange={(message) => setMessage(message)}
+        onFileSelect={(files) => setFiles(files)}
+      />
     </IonPage>
-  ) : <IonLoading isOpen={loading} />;
+  ) : (
+    <IonLoading isOpen={loading} />
+  );
 };
 
 export default GroupChat;
