e044afa2e8ec22a696a0f29b0df6ef7e72a6f2c6
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-04-16 16:38:22 +0900

    added serde cases

--- dnas/lobby/zomes/aggregator/src/types.rs
+++ dnas/lobby/zomes/aggregator/src/types.rs
@@ -1,8 +1,8 @@
+use file_types::Payload;
 use hdk::prelude::*;
 use hdk::prelude::{element::SignedHeaderHashed, timestamp::Timestamp};
-use file_types::Payload;
-use std::time::SystemTime;
 use std::collections::hash_map::HashMap;
+use std::time::SystemTime;
 
 // for contacts
 #[derive(Deserialize, Serialize, SerializedBytes, Debug)]
@@ -10,6 +10,7 @@ pub struct AgentPubKeys(pub Vec<AgentPubKey>);
 
 // for username
 #[derive(Serialize, Deserialize, Debug, SerializedBytes)]
+#[serde(rename_all = "camelCase")]
 pub struct UsernameInfo {
     username: String,
     agent_id: AgentPubKey,
@@ -22,6 +23,7 @@ pub struct UsernameList(Vec<UsernameInfo>);
 
 // for group
 #[derive(Deserialize, Serialize, SerializedBytes, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct GroupOutput {
     pub group_id: EntryHash,
     pub group_revision_id: HeaderHash,
