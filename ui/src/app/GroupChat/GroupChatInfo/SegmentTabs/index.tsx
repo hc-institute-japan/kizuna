06e517d6ac28bd2319773b57a012d52eb642d710
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-05-23 18:48:36 +0900

    group slide bug fixed, code clean up for group

--- ui/src/app/GroupChat/GroupChatInfo/SegmentTabs/index.tsx
+++ ui/src/app/GroupChat/GroupChatInfo/SegmentTabs/index.tsx
@@ -4,27 +4,28 @@ import { useIntl } from "react-intl";
 
 interface Props {
 	onSegmentChange(index: any): any
+  value: string
 }
 
 /*
 	displays the tab segments in GroupChat detail page
 	which controls the displayed slides
 */
-const SegmentTabs: React.FC<Props> = ({ onSegmentChange }) => {
+const SegmentTabs: React.FC<Props> = ({ onSegmentChange, value }) => {
 	let intl = useIntl();
 
 	return (				
     <IonToolbar>
-      <IonSegment value="info" onIonChange={(e) => onSegmentChange(e.detail.value)}>
-        <IonSegmentButton value="info">
+      <IonSegment value={value} onIonChange={(e) => onSegmentChange(e.detail.value)}>
+        <IonSegmentButton value="Info">
           <IonLabel>{intl.formatMessage({ id: "app.group-chat.label-info" })}</IonLabel>
         </IonSegmentButton>
       
-        <IonSegmentButton value="media">
+        <IonSegmentButton value="Media">
           <IonLabel>{intl.formatMessage({ id: "app.group-chat.label-media" })}</IonLabel>
         </IonSegmentButton>
       
-        <IonSegmentButton value="files">
+        <IonSegmentButton value="Files">
           <IonLabel>{intl.formatMessage({ id: "app.group-chat.label-files" })}</IonLabel>
         </IonSegmentButton>
       </IonSegment>
