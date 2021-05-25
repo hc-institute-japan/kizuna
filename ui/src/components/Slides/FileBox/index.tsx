8e3719d80747a4715ede00a3ecbf41d1be7340f5
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-05-25 14:48:37 +0900

    Media Box component implemented
    TODO: remove the dispatch remaining in the component as well as fix the appending of "u" in the hash for it to be reusable both in group and p2p

--- ui/src/components/Slides/FileBox/index.tsx
+++ ui/src/components/Slides/FileBox/index.tsx
@@ -3,7 +3,9 @@ import {
   IonCol,
   IonGrid,
   IonInfiniteScroll,
-  IonInfiniteScrollContent, IonRow, IonSlide
+  IonInfiniteScrollContent,
+  IonRow,
+  IonSlide,
 } from "@ionic/react";
 import React, { useRef } from "react";
 import { useIntl } from "react-intl";
@@ -16,74 +18,82 @@ import FileView from "../../Chat/File/FileView";
 import styles from "../style.module.css";
 
 interface Props {
-	conversant: Profile,
-	orderedFileMessages: P2PMessage[] | GroupMessage[],
-	onDownload(file: FilePayload): any;
-  onScrollBottom(complete: () => Promise<void>, files: P2PMessage[] | GroupMessage[]): any;
+  conversant: Profile;
+  orderedFileMessages: P2PMessage[] | GroupMessage[];
+  onDownload(file: FilePayload): any;
+  onScrollBottom(
+    complete: () => Promise<void>,
+    files: P2PMessage[] | GroupMessage[]
+  ): any;
 }
 
 /* displays the grid of files */
-const FileBox: React.FC<Props> = ({ orderedFileMessages, onDownload, onScrollBottom }) => {
-	/* i18n */
-	let intl = useIntl();
+const FileBox: React.FC<Props> = ({
+  orderedFileMessages,
+  onDownload,
+  onScrollBottom,
+}) => {
+  /* i18n */
+  const intl = useIntl();
 
-	/* REFS */
-	const infiniteFileScroll = useRef<HTMLIonInfiniteScrollElement>(null);
-	const complete = () => infiniteFileScroll.current!.complete();
+  /* REFS */
+  const infiniteFileScroll = useRef<HTMLIonInfiniteScrollElement>(null);
+  const complete = () => infiniteFileScroll.current!.complete();
 
-	// determines the size of the grid columns
-	// whole page width for others (looks like list)
-	
-	// helper code for displaying date
+  // determines the size of the grid columns
+  // whole page width for others (looks like list)
+
+  // helper code for displaying date
   let currentMonth = -1;
 
-	/* RENDER */
-	return (
-		<IonSlide>
-			<IonGrid>
-				<IonRow className={styles.mediarow}>
-					{orderedFileMessages.map((message: P2PMessage | GroupMessage) => {
-						let month = message.timestamp.getMonth();
-						// let year = file.timestamp.getFullYear();
-						return (
-							<React.Fragment>
-								{month !== currentMonth
-									? ((currentMonth = month),
-										(
-											<IonCol size="12">
-												<h2 className={styles.month}>
-													{monthToString(month, intl)}
-												</h2>
-											</IonCol>
-										))
-									: null}
-								<IonCol size="12">    
-									<IonCard className={styles.mediacard}>
-                      <FileView 
-												file={message.payload as FilePayload} 
-												onDownload={onDownload}
-											/>
-									</IonCard>
-								</IonCol>
-							</React.Fragment>
-						);
-					})}
-				</IonRow>
+  /* RENDERER */
+  const renderMonth = (month: number) =>
+    month !== currentMonth
+      ? ((currentMonth = month),
+        (
+          <IonCol size="12">
+            <h2 className={styles.month}>{monthToString(month, intl)}</h2>
+          </IonCol>
+        ))
+      : null;
 
-				<IonRow>
-					<IonInfiniteScroll
-						ref={infiniteFileScroll}
-						position="bottom"
-						onIonInfinite={(e) => onScrollBottom(complete, orderedFileMessages)}
-					>
-						<IonInfiniteScrollContent></IonInfiniteScrollContent>
-					</IonInfiniteScroll>
-				</IonRow>
+  const renderFileMessages = () =>
+    orderedFileMessages.map((message: P2PMessage | GroupMessage) => {
+      let month = message.timestamp.getMonth();
+      // let year = file.timestamp.getFullYear();
+      return (
+        <React.Fragment>
+          {renderMonth(month)}
+          <IonCol size="12">
+            <IonCard className={styles.mediacard}>
+              <FileView
+                file={message.payload as FilePayload}
+                onDownload={onDownload}
+              />
+            </IonCard>
+          </IonCol>
+        </React.Fragment>
+      );
+    });
 
-			</IonGrid>
-		</IonSlide>
-	)
-}
+  /* RENDER */
+  return (
+    <IonSlide>
+      <IonGrid>
+        <IonRow className={styles.mediarow}>{renderFileMessages()}</IonRow>
+
+        <IonRow>
+          <IonInfiniteScroll
+            ref={infiniteFileScroll}
+            position="bottom"
+            onIonInfinite={(e) => onScrollBottom(complete, orderedFileMessages)}
+          >
+            <IonInfiniteScrollContent></IonInfiniteScrollContent>
+          </IonInfiniteScroll>
+        </IonRow>
+      </IonGrid>
+    </IonSlide>
+  );
+};
 
 export default FileBox;
-
