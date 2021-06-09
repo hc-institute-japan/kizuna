acfdac5aa91cbe4d6664b81176dfad3454116d64
seulgibear <neilsongardose@gmail.com>
2021-06-09 10:48:53 +0800

    #48 Add spinner when fetching username instead of showing unauthenticated route

--- ui/src/app/Auth/index.tsx
+++ ui/src/app/Auth/index.tsx
@@ -1,20 +1,31 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { IonSpinner } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { fetchMyUsername } from "../../redux/profile/actions";
 import { RootState } from "../../redux/types";
+import { useAppDispatch } from "../../utils/helpers";
 import Authenticated from "../Authenticated";
 import Unauthenticated from "../Unauthenticated";
 
 const Auth: React.FC = () => {
   const { username } = useSelector((state: RootState) => state.profile);
-  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(fetchMyUsername());
+    dispatch(fetchMyUsername()).then((bool: boolean) => {
+      setLoading(false);
+    });
   }, [dispatch]);
 
   /* TODO: Maybe better to load here while username is getting fetched */
-  return username !== null ? <Authenticated /> : <Unauthenticated />;
+  return loading ? (
+    <IonSpinner />
+  ) : username !== null ? (
+    <Authenticated />
+  ) : (
+    <Unauthenticated />
+  );
 };
 
 export default Auth;
