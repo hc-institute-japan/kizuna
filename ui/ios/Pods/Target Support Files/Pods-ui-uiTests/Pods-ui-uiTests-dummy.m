859e300443cb52d7c07ed869401afee3b60e82d7
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-06-10 19:01:36 +0900

    remove pods

--- ui/ios/Pods/Target Support Files/Pods-ui-uiTests/Pods-ui-uiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ui_uiTests : NSObject
-@end
-@implementation PodsDummy_Pods_ui_uiTests
-@end
--- ui/ios/Pods/Target Support Files/Pods-ui/Pods-ui-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ui : NSObject
-@end
-@implementation PodsDummy_Pods_ui
-@end
