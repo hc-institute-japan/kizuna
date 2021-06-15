4345c0df7d02fa62ec381105d744e9741e625dcd
seulgibear <neilsongardose@gmail.com>
2021-06-15 10:56:36 +0800

    Implement auto calculation of toast error duration for english based on number of words

--- ui/src/containers/ToastContainer/index.tsx
+++ ui/src/containers/ToastContainer/index.tsx
@@ -1,11 +1,14 @@
-import { ToastOptions } from "@ionic/core";
+import { IonicSafeString, ToastOptions } from "@ionic/core";
 import { IonToast } from "@ionic/react";
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/types";
 import { ToastContext } from "./context";
 
 const ToastContainer: React.FC = ({ children }) => {
   const [isShowing, setIsShowing] = useState(false);
   const [shouldShow, setShouldShow] = useState(false);
+  const language = useSelector((state: RootState) => state.language.language);
   const [props, setProps] = useState({});
   const showToast = (props: ToastOptions) => {
     setProps(props);
@@ -25,8 +28,20 @@ const ToastContainer: React.FC = ({ children }) => {
     if (!shouldShow) setProps({});
   }, [shouldShow]);
 
+  function isString(string: any): string is string {
+    return string.split !== undefined;
+  }
+
   const showErrorToast = (props: ToastOptions) => {
-    setProps({ color: "danger", ...props });
+    if (language === "en") {
+      const message = props?.message;
+      if (message) {
+        const duration = isString(message)
+          ? (message as string).split(" ").length * 160
+          : (message as IonicSafeString).value.split(" ").length * 160;
+        setProps({ color: "danger", duration, ...props });
+      }
+    } else setProps({ color: "danger", ...props });
     setShouldShow(true);
   };
 
