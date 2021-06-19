bbd10a5b90f7363e6a555897482ce888c6bacb44
Tatsuya Sato <tatsuya.g.sato@yumeville.com>
2021-06-19 17:23:20 +0900

    update nix config

--- default.nix
+++ default.nix
@@ -7,10 +7,10 @@ let
     includeHolochainBinaries = true;
     holochainVersionId = "custom";
     
-    holochainVersion = { 
-     rev = "a6ac0439670ba367c723a80d3b8bc7c419aa5f6e";
-     sha256 = "16a9mjq8g40mp5120714ff5wpks74n3n39liswikqqf1sxabza9c";
-     cargoSha256 = "0sbr6ag5dyv7gdchpp5zjwzbsr1ggvxnmik21wm71gjd8hhwcdy6";
+    holochainVersion = {
+     rev = "24ceb63bdea374d1936b723e1966caf2e55ebfdc";
+     sha256 = "16hsikyasi0zbh7gfrpzlahydx7csnvshz421sx56f0jpwvi2g80";
+     cargoSha256 = "sha256:0w29y8w5k5clq74k84ksj5aqxbxhqxh2djhll6vv694djw277rpj";
      bins = {
        holochain = "holochain";
        hc = "hc";
