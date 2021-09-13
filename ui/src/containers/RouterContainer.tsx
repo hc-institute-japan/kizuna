6e6c44aa9c7b438319e1d130e905661ce4d2c29c
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-09-13 11:55:49 +0100

    reset routing

--- ui/src/containers/RouterContainer.tsx
+++ ui/src/containers/RouterContainer.tsx
@@ -1,11 +1,8 @@
 import { IonReactRouter } from "@ionic/react-router";
-import { BrowserRouter } from "react-router-dom";
 import React from "react";
 
 const RouterContainer: React.FC = ({ children }) => (
-  <IonReactRouter>
-    <BrowserRouter>{children}</BrowserRouter>
-  </IonReactRouter>
+  <IonReactRouter>{children}</IonReactRouter>
 );
 
 export default RouterContainer;
