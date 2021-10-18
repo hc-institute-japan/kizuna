7c55e97698f38c1bd00e8e7ca388c4a6b5352ccc
Nicko Pangarungan <nickolie.pangarungan@gmail.com>
2021-10-19 00:54:25 +0800

    change map into for loops in parallel calls

--- dnas/lobby/zomes/group/src/entries/group/get_all_my_groups.rs
+++ dnas/lobby/zomes/group/src/entries/group/get_all_my_groups.rs
@@ -1,4 +1,3 @@
-#[allow(dead_code)]
 use hdk::prelude::*;
 
 use super::group_helpers::get_group_latest_version;
@@ -8,13 +7,10 @@ use crate::utils::error;
 pub fn get_all_my_groups_handler() -> ExternResult<Vec<GroupOutput>> {
     let my_pub_key: AgentPubKey = agent_info()?.agent_latest_pubkey;
     let mut my_linked_groups_entries: Vec<GroupOutput> = vec![];
-    // let mut group_id: EntryHash;
-    // let mut group_revision_id: HeaderHash;
-    // let mut group: Group;
 
     let links = get_links(my_pub_key.into(), Some(LinkTag::new("member")))?;
 
-    let get_input = links
+    let get_input: Vec<GetInput> = links
         .into_inner()
         .into_iter()
         .map(|link| GetInput::new(link.target.into(), GetOptions::latest()))
@@ -22,92 +18,49 @@ pub fn get_all_my_groups_handler() -> ExternResult<Vec<GroupOutput>> {
 
     let get_output = HDK.with(|h| h.borrow().get_details(get_input))?;
 
-    let _get_output_result = get_output
+    let get_output_result: Vec<Details> = get_output
         .into_iter()
-        .filter_map(|maybe_option| maybe_option) // returns value in Some(value)
-        .map(|details| {
-            match details {
-                Details::Entry(group_entry_details) => {
-                    if let Entry::App(group_entry_bytes) = group_entry_details.entry {
-                        let group_sb: SerializedBytes = group_entry_bytes.into_sb();
-                        let first_ver_group: Group = group_sb.try_into()?;
-                        // get original value of created and creator here
-                        let group_id = hash_entry(first_ver_group.clone())?;
-                        let mut group = first_ver_group;
+        .filter_map(|maybe_option| maybe_option)
+        .collect();
 
-                        // This is the SignedHeaderHashed of the sole Create Header of Group
-                        let group_signed_header_hashed: SignedHeaderHashed =
-                            group_entry_details.headers[0].to_owned();
+    for details in get_output_result {
+        debug!("nicko get all my groups map details {:?}", details.clone());
+        match details {
+            Details::Entry(group_entry_details) => {
+                debug!("nicko get all my groups reaches inside match statement of details entry",);
 
-                        let group_revision_id =
-                            group_signed_header_hashed.header_address().to_owned(); // This is the create header hash of Group
+                if let Entry::App(group_entry_bytes) = group_entry_details.entry {
+                    let group_sb: SerializedBytes = group_entry_bytes.into_sb();
+                    let first_ver_group: Group = group_sb.try_into()?;
+                    // get original value of created and creator here
+                    let group_id = hash_entry(first_ver_group.clone())?;
+                    let mut group = first_ver_group;
 
-                        if !group_entry_details.updates.is_empty() {
-                            let latest_group: GroupOutput =
-                                get_group_latest_version(group_id.clone())?;
+                    // This is the SignedHeaderHashed of the sole Create Header of Group
+                    let group_signed_header_hashed: SignedHeaderHashed =
+                        group_entry_details.headers[0].to_owned();
 
-                            group.name = latest_group.latest_name; // latest group name
+                    let group_revision_id = group_signed_header_hashed.header_address().to_owned(); // This is the create header hash of Group
 
-                            group.members = latest_group.members; // latest group members
-                        }
+                    if !group_entry_details.updates.is_empty() {
+                        let latest_group: GroupOutput = get_group_latest_version(group_id.clone())?;
 
-                        my_linked_groups_entries.push(GroupOutput::new(
-                            group.clone(),
-                            group_id.clone(),
-                            group_revision_id.clone(),
-                        ));
-                    } else {
-                        return error("this is a fatal error. Something is wrong with holochain.");
+                        group.name = latest_group.latest_name; // latest group name
+                        group.members = latest_group.members; // latest group members
                     }
 
-                    Ok(())
+                    my_linked_groups_entries.push(GroupOutput::new(
+                        group.clone(),
+                        group_id.clone(),
+                        group_revision_id.clone(),
+                    ));
+                } else {
+                    return error("this is a fatal error. Something is wrong with holochain.");
                 }
-                _ => Ok(()),
             }
-        });
-    // .collect();
+            _ => (),
+        }
+    }
 
-    // for link in get_links(my_pub_key.into(), Some(LinkTag::new("member")))?.into_inner() {
-    //     if let Some(details) = get_details(link.target.clone(), GetOptions::latest())? {
-    //         match details {
-    //             Details::Entry(group_entry_details) => {
-    //                 // This is the SignedHeaderHashed of the sole Create Header of Group
-    //                 let group_signed_header_hashed: SignedHeaderHashed =
-    //                     group_entry_details.headers[0].to_owned();
-
-    //                 group_id = link.target.clone();
-    //                 group_revision_id = group_signed_header_hashed.header_address().to_owned(); // This is the create header hash of Group
-
-    //                 if let Entry::App(group_entry_bytes) = group_entry_details.entry {
-    //                     let group_sb: SerializedBytes = group_entry_bytes.into_sb();
-    //                     let first_ver_group: Group = group_sb.try_into()?;
-    //                     // get original value of created and creator here
-    //                     group = first_ver_group;
-    //                 } else {
-    //                     return error("this is a fatal error. Something is wrong with holochain.");
-    //                 }
-
-    //                 if !group_entry_details.updates.is_empty() {
-    //                     let latest_group: GroupOutput =
-    //                         get_group_latest_version(link.target.clone())?;
-
-    //                     group.name = latest_group.latest_name; // latest group name
-
-    //                     group.members = latest_group.members; // latest group members
-    //                 }
-
-    //                 my_linked_groups_entries.push(GroupOutput::new(
-    //                     group,
-    //                     group_id,
-    //                     group_revision_id,
-    //                 ));
-    //             }
-    //             _ => (),
-    //         }
-    //     }
-    // }
-
-    let output = my_linked_groups_entries;
-
-    Ok(output)
+    Ok(my_linked_groups_entries)
 }
--- dnas/lobby/zomes/group/src/entries/group_message/get_files_bytes.rs
+++ dnas/lobby/zomes/group/src/entries/group_message/get_files_bytes.rs
@@ -16,46 +16,25 @@ pub fn get_files_bytes_handler(
 
     let get_output = HDK.with(|h| h.borrow().get(get_input))?;
 
-    let _get_output_result = get_output
+    let get_output_result: Vec<Element> = get_output
         .into_iter()
         .filter_map(|maybe_option| maybe_option)
-        .map(|file_bytes_element| {
-            match file_bytes_element.entry().to_app_option::<GroupFileBytes>() {
-                Ok(option) => match option {
-                    Some(file_bytes) => {
-                        let file_hash = hash_entry(file_bytes.clone())?;
-                        all_file_bytes.insert(file_hash.to_string(), file_bytes.0);
-                        Ok(())
-                    }
-                    None => Ok(()),
-                },
-                Err(_) => {
-                    return error(
-                        "the group file bytes ElementEntry enum is not of Present variant",
-                    );
-                }
-            }
-        });
+        .collect();
 
-    /* We are only concerned with the content and not the metadata here */
-    // for file_hash in file_hashes {
-    //     if let Some(file_bytes_element) = get(file_hash.clone(), GetOptions::content())? {
-    //         match file_bytes_element.entry().to_app_option::<GroupFileBytes>() {
-    //             Ok(option) => match option {
-    //                 Some(file_bytes) => {
-    //                     all_file_bytes.insert(file_hash.to_string(), file_bytes.0);
-    //                 }
-    //                 None => {}
-    //             },
-    //             Err(_) => {
-    //                 return error(
-    //                     "the group file bytes ElementEntry enum is not of Present variant",
-    //                 );
-    //             }
-    //         }
-    //     } else {
-    //         return error("The file bytes were not found");
-    //     }
-    // }
+    for file_bytes_element in get_output_result {
+        match file_bytes_element.entry().to_app_option::<GroupFileBytes>() {
+            Ok(option) => match option {
+                Some(file_bytes) => {
+                    let file_hash = hash_entry(file_bytes.clone())?;
+                    all_file_bytes.insert(file_hash.to_string(), file_bytes.0);
+                }
+                None => (),
+            },
+            Err(_) => {
+                return error("the group file bytes ElementEntry enum is not of Present variant");
+            }
+        }
+    }
+
     Ok(all_file_bytes)
 }
