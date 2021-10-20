6ecb244193bc5bd6d0c10342ff7fb86e6537602f
Nicko Pangarungan <nickolie.pangarungan@gmail.com>
2021-10-20 20:42:07 +0800

    refactor get_links in get_messages_by_group_by_timestamp to parallel call

--- dnas/lobby/zomes/group/src/entries/group_message/get_messages_by_group_by_timestamp.rs
+++ dnas/lobby/zomes/group/src/entries/group_message/get_messages_by_group_by_timestamp.rs
@@ -38,58 +38,102 @@ pub fn get_messages_by_group_by_timestamp_handler(
             let mut group_messages_content: HashMap<String, GroupMessageContent> = HashMap::new();
             let mut messages_hashes: Vec<EntryHash> = Vec::new();
 
+            let mut all_read_list: HashMap<String, HashMap<String, Timestamp>> = HashMap::new();
+            let mut message_hashes_for_read = Vec::new();
+            let mut timestamps = Vec::new();
+
             let links = message_links.into_inner();
+
+            // create input for getting the messages
             let get_input = links
-                // .into_inner()
+                .clone()
                 .into_iter()
                 .map(|link| GetInput::new(link.target.into(), GetOptions::default()))
                 .collect();
 
+            // parallel get messages
             let get_output = HDK.with(|h| h.borrow().get(get_input))?;
-
             let get_output_result: Vec<Element> = get_output
                 .into_iter()
                 .filter_map(|maybe_option| maybe_option)
                 .collect();
+
+            // create input for getting the read links => input: Vec<(message_hash, "read")>
+            let get_input_message_hashes: Vec<GetLinksInput> = links
+                .clone()
+                .into_iter()
+                .map(|link| {
+                    GetLinksInput::new(link.target.into(), Some(LinkTag::new("read".to_owned())))
+                })
+                .collect();
+
+            // parallel get links
+            let read_links: Vec<Link> = HDK
+                .with(|h| h.borrow().get_links(get_input_message_hashes))?
+                .into_iter()
+                .map(|links| links.into_inner())
+                .flatten()
+                .collect();
+
+            let get_input_for_read: Vec<GetInput> = read_links
+                .into_iter()
+                .map(|link| {
+                    timestamps.push(link.timestamp);
+                    let message_hash: EntryHash = link.target.clone().into();
+                    message_hashes_for_read.push(message_hash);
+                    GetInput::new(link.target.into(), GetOptions::default())
+                })
+                .collect();
+
+            let get_output_for_read = HDK.with(|h| h.borrow().get(get_input_for_read))?;
+
+            let zipped: Vec<((Element, EntryHash), Timestamp)> = get_output_for_read
+                .into_iter()
+                .zip(message_hashes_for_read)
+                .zip(timestamps)
+                .filter_map(|((element_maybe_option, message_hash), timestamp)| {
+                    match element_maybe_option {
+                        Some(element) => {
+                            return Some(((element, message_hash), timestamp.to_owned()))
+                        }
+                        None => return None,
+                    }
+                })
+                .collect();
+
+            for ((element, message_hash), timestamp) in zipped.into_iter() {
+                match element.into_inner().1 {
+                    ElementEntry::Present(entry) => {
+                        if let Entry::Agent(agent_pubkey) = entry {
+                            match all_read_list.get_mut(&message_hash.clone().to_string()) {
+                                Some(hashmap) => {
+                                    hashmap.insert(
+                                        agent_pubkey.clone().to_string(),
+                                        timestamp.to_owned(),
+                                    );
+                                    ()
+                                }
+                                None => {
+                                    let mut new = HashMap::new();
+                                    new.insert(
+                                        agent_pubkey.clone().to_string(),
+                                        timestamp.to_owned(),
+                                    );
+                                    all_read_list.insert(message_hash.clone().to_string(), new);
+                                    ()
+                                }
+                            };
+                        }
+                    }
+                    _ => (),
+                };
+            }
 
             for element in get_output_result {
                 match element.entry().to_owned().to_app_option::<GroupMessage>() {
                     Ok(option) => match option {
                         Some(group_message) => {
                             let message_hash = hash_entry(group_message.clone())?;
-
-                            let read_links = get_links(
-                                message_hash.clone(),
-                                Some(LinkTag::new("read".to_owned())),
-                            )?;
-                            let mut read_list: HashMap<String, Timestamp> = HashMap::new();
-
-                            let mut timestamps = Vec::new();
-
-                            let get_input_for_read = read_links
-                                .into_inner()
-                                .into_iter()
-                                .map(|link| {
-                                    timestamps.push(link.timestamp);
-                                    GetInput::new(link.target.into(), GetOptions::default())
-                                })
-                                .collect();
-
-                            let get_output_for_read =
-                                HDK.with(|h| h.borrow().get(get_input_for_read))?;
-
-                            let _get_output_for_read_result = get_output_for_read
-                                .into_iter()
-                                .filter_map(|maybe_option| maybe_option)
-                                .zip(timestamps)
-                                .map(|(element, timestamp)| match element.into_inner().1 {
-                                    ElementEntry::Present(entry) => {
-                                        if let Entry::Agent(agent_pubkey) = entry {
-                                            read_list.insert(agent_pubkey.to_string(), timestamp);
-                                        }
-                                    }
-                                    _ => (),
-                                });
 
                             let mut group_message_data = GroupMessageData {
                                 message_id: message_hash.clone(),
@@ -120,7 +164,13 @@ pub fn get_messages_by_group_by_timestamp_handler(
                                 message_hash.clone().to_string(),
                                 GroupMessageContent {
                                     group_message_element,
-                                    read_list: read_list,
+                                    // read_list: read_list,
+                                    read_list: match all_read_list
+                                        .get(&message_hash.clone().to_string())
+                                    {
+                                        Some(hashmap) => hashmap.to_owned(),
+                                        None => HashMap::new(),
+                                    },
                                 },
                             );
 
