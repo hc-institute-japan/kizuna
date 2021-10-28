d61258d7969ce6d2d5567e9173dabfb2c4015fe1
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-10-28 21:09:28 +0900

    implement err page when profile failed to fetch

--- ui/src/app/Auth/index.tsx
+++ ui/src/app/Auth/index.tsx
@@ -1,4 +1,7 @@
+import { IonIcon, IonText } from "@ionic/react";
+import { sadOutline } from "ionicons/icons";
 import React, { useEffect, useState } from "react";
+import { useIntl } from "react-intl";
 import { useSelector } from "react-redux";
 import Spinner from "../../components/Spinner";
 import { getMyProfile } from "../../redux/profile/actions";
@@ -6,24 +9,38 @@ import { RootState } from "../../redux/types";
 import { useAppDispatch } from "../../utils/helpers";
 import Authenticated from "../Authenticated";
 import Unauthenticated from "../Unauthenticated";
+import styles from "./style.module.css";
 
 const Auth: React.FC = () => {
+  const intl = useIntl();
   const { username } = useSelector((state: RootState) => state.profile);
   const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(false);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getMyProfile()).then((bool: boolean) => {
+      console.log(bool);
+      if (!bool) setErr(true);
       setLoading(false);
     });
   }, [dispatch]);
+
+  const renderErrPage = () => (
+    <div className={styles["err"]}>
+      <IonIcon icon={sadOutline} />
+      <IonText>{intl.formatMessage({ id: "app.auth.err" })}</IonText>
+    </div>
+  );
 
   return loading ? (
     <Spinner />
   ) : username !== null ? (
     <Authenticated />
-  ) : (
+  ) : !err ? (
     <Unauthenticated />
+  ) : (
+    renderErrPage()
   );
   // return <Playground />;
 };
