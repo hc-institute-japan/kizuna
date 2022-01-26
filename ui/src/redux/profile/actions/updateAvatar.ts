b3fb3de01d8526282be38499cab3756c5fde1c7a
seulgibear <neilsongardose@gmail.com>
2022-01-27 00:45:39 +0800

    Implement update profiles avatar

---
+++ ui/src/redux/profile/actions/updateAvatar.ts
@@ -0,0 +1,36 @@
+import { serializeHash } from "@holochain-open-dev/core-types";
+import { FUNCTIONS, ZOMES } from "../../../connection/types";
+import { binaryToUrl } from "../../../utils/helpers";
+import { ThunkAction } from "../../types";
+import { ProfileActionTypes, SET_PROFILE } from "../types";
+
+export const updateAvatar =
+  (avatar: Uint8Array): ThunkAction =>
+  async (dispatch, getState, { getAgentId, callZome }) => {
+    const myAgentId = await getAgentId();
+    const { username, fields } = getState().profile;
+    /* assume that getAgentId() is non-nullable */
+    const myAgentIdB64 = serializeHash(myAgentId!);
+    const serializedAvatar = serializeHash(avatar);
+
+    const updatedFields = { ...fields, avatar: serializedAvatar };
+
+    await callZome({
+      zomeName: ZOMES.PROFILES,
+      fnName: FUNCTIONS[ZOMES.PROFILES].UPDATE_PROFILE,
+      payload: {
+        nickname: username,
+        fields: updatedFields,
+      },
+    });
+
+    dispatch<ProfileActionTypes>({
+      type: SET_PROFILE,
+      id: myAgentIdB64,
+      /* assert that nickname is non-nullable */
+      nickname: username!,
+      fields: { ...updatedFields, avatar: binaryToUrl(serializedAvatar) },
+    });
+  };
+
+export default updateAvatar;
