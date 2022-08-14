8001de124b66858ba8d3e18a2c8952b92c30f370
tatsuya sato <tatsuya.g.sato@yumeville.com>
2022-08-14 17:20:42 +0900

    fix getMyProfile

--- ui/src/redux/profile/actions/getMyProfile.ts
+++ ui/src/redux/profile/actions/getMyProfile.ts
@@ -1,4 +1,3 @@
-import { serializeHash } from "@holochain-open-dev/core-types";
 import { decode } from "@msgpack/msgpack";
 import {
   binaryToUrl,
@@ -13,35 +12,31 @@ import { ProfileActionTypes, ProfileRaw, SET_PROFILE } from "../types";
 
 const getMyProfile =
   (): ThunkAction =>
-  async (dispatch, getState, { callZome, getAgentId }) => {
-    console.log("calling get my profile");
-    try {
-      const res = await callZome({
-        zomeName: ZOMES.PROFILES,
-        fnName: FUNCTIONS[ZOMES.PROFILES].GET_MY_PROFILE,
-      });
+    async (dispatch, getState, { callZome, getAgentId }) => {
+      console.log("calling get my profile");
+      try {
+        const res = await callZome({
+          zomeName: ZOMES.PROFILES,
+          fnName: FUNCTIONS[ZOMES.PROFILES].GET_MY_PROFILE,
+        });
 
-      const myAgentIdB64 = getState().profile.id!; // find another source for this
-      const id = serializeHash(res.signed_action.Create.author);
-
-      console.log("myAgentId", id);
-
-      if (res) {
-        const profileRaw = decode(getEntryFromRecord(res)) as ProfileRaw;
-        dispatch<ProfileActionTypes>({
-          type: SET_PROFILE,
-          nickname: profileRaw.nickname,
-          id: myAgentIdB64,
-          fields: profileRaw.fields.avatar
-            ? {
+        const myAgentIdB64 = getState().profile.id!; // find another source for this
+        if (res) {
+          const profileRaw = decode(getEntryFromRecord(res)) as ProfileRaw;
+          dispatch<ProfileActionTypes>({
+            type: SET_PROFILE,
+            nickname: profileRaw.nickname,
+            id: myAgentIdB64,
+            fields: profileRaw.fields.avatar
+              ? {
                 avatar: binaryToUrl(profileRaw.fields.avatar),
               }
-            : {},
-        });
-      }
-      return true;
-    } catch (e) {}
-    return false;
-  };
+              : {},
+          });
+        }
+        return true;
+      } catch (e) { }
+      return false;
+    };
 
 export default getMyProfile;
